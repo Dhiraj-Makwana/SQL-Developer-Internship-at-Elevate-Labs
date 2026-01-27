2fff11ae7a532a4e19c071744225aa0c1901c41c
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2026-01-27 16:42:00 +0530

    Update Task-7: implement ON DELETE CASCADE

--- Task-7/task-7.sql
+++ Task-7/task-7.sql
@@ -18,6 +18,8 @@ VALUES
 
 SELECT * FROM employees;
 SELECT * FROM departments;
+DESCRIBE employees;
+DESCRIBE departments;
 
 UPDATE employees
 SET department_id = 1
@@ -26,3 +28,34 @@ WHERE employee_id IN (1, 4);
 UPDATE employees
 SET department_id = 2
 WHERE employee_id = 2;
+
+ALTER TABLE employees
+ADD FOREIGN KEY (department_id) REFERENCES departments(department_id);
+
+INSERT INTO employees
+(first_name, last_name, email, salary, hire_date, department_id)
+VALUES
+('Dhiraj', 'Makwana', 'dhiraj.makwana@company.com', 55000, '2026-02-01', 1);
+
+UPDATE employees
+SET department = 'IT'
+WHERE employee_id = 6;
+
+INSERT INTO employees
+(first_name, last_name, email, salary, hire_date, department_id)
+VALUES
+('Rishi', 'Patel', 'rishi.patel@company.com', 55000, '2026-02-01', 4);
+
+SHOW CREATE TABLE employees;
+
+ALTER TABLE employees
+DROP FOREIGN KEY employees_ibfk_1;
+
+ALTER TABLE employees
+ADD CONSTRAINT fk_employee_department
+FOREIGN KEY (department_id)
+REFERENCES departments(department_id)
+ON DELETE CASCADE;
+
+DELETE FROM departments
+WHERE department_id = 1;
